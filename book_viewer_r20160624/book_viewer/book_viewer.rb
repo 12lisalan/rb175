cf67ef3128a972aeef4906701adbaf3318ac1eb3
Lisa Lan <12lisalan@gmail.com>
2023-11-05 01:52:37 +0000

    chapter 1

--- book_viewer_r20160624/book_viewer/book_viewer.rb
+++ book_viewer_r20160624/book_viewer/book_viewer.rb
@@ -3,7 +3,7 @@ require "sinatra/reloader"
 require "tilt/erubis"
 
 get "/" do
-  @title = "Some title"
+  @title = "The Adventures of Sherlock Holmes"
   @content = File.readlines("data/toc.txt")
   erb :home
 end
@@ -11,6 +11,6 @@ end
 get "/chapters/1" do
   @title = "Chapter 1"
   @content = File.readlines("data/toc.txt")
-  @chapter_1 = File.readlines("data/chp1.txt")
+  @chapter_1 = File.read("data/chp1.txt")
   erb :chapter
 end
