1c7f16e770af3c0ca450751eb6d93266ae0275c0
Lisa Lan <12lisalan@gmail.com>
2023-11-06 06:07:56 +0000

    layout

--- book_viewer_r20160624/book_viewer/book_viewer.rb
+++ book_viewer_r20160624/book_viewer/book_viewer.rb
@@ -4,13 +4,13 @@ require "tilt/erubis"
 
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @content = File.readlines("data/toc.txt")
+  @contents = File.readlines("data/toc.txt")
   erb :home
 end
 
 get "/chapters/1" do
   @title = "Chapter 1"
-  @content = File.readlines("data/toc.txt")
-  @chapter_1 = File.read("data/chp1.txt")
+  @contents = File.readlines("data/toc.txt")
+  @chapter = File.read("data/chp1.txt")
   erb :chapter
 end
--- directory/directory.rb
+++ directory/directory.rb
@@ -3,5 +3,11 @@ require "sinatra/reloader"
 require "tilt/erubis"
 
 get "/" do
-  File.read "public/hello.html"
+  @entries = Dir.entries("public").reject{|x| x[0] == "."}
+  erb :home
+end
+
+get "/descending" do
+  @entries = Dir.entries("public").reject{|x| x[0] == "."}
+  erb :descending
 end
