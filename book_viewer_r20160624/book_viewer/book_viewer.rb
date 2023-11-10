60291f444be3eaef46bfc3c388656cc6282b7d59
Lisa Lan <12lisalan@gmail.com>
2023-11-10 02:42:26 +0000

    search

--- book_viewer_r20160624/book_viewer/book_viewer.rb
+++ book_viewer_r20160624/book_viewer/book_viewer.rb
@@ -2,15 +2,58 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+helpers do
+  def in_paragraphs(chapter)
+    chapter.split("\n\n").map.with_index do |paragraph, index|
+      "<p id='#{index+1}'>#{paragraph}</p>"
+    end.join
+  end
+end
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines("data/toc.txt")
+
   erb :home
 end
 
-get "/chapters/1" do
-  @title = "Chapter 1"
-  @contents = File.readlines("data/toc.txt")
-  @chapter = File.read("data/chp1.txt")
+get "/chapters/:number" do
+  number = params[:number].to_i
+  chapter_name = @contents[number - 1]
+
+  redirect "/" unless (1..@contents.size).cover? number
+
+  @title = "Chapter #{number}: #{chapter_name}"
+  @chapter = File.read("data/chp#{number}.txt")
+
   erb :chapter
+end
+
+
+get "/search" do
+  query = params[:query]
+  @result = search
+
+  erb :search
+end
+
+not_found do
+  redirect "/"
+end
+
+# returns hash in format {number:title} of chapters containing
+# search term
+def search
+  query = params[:query]
+  result = {}
+  @contents.each_with_index do |title, number|
+    number = number + 1
+    chapter = File.read("data/chp#{number}.txt")
+    if chapter.include?(query)
+      result[number] = title
+    end
+  end
+  result
 end
