af3addd155549eed8d475a346deb49227bca4f64
Lisa Lan <12lisalan@gmail.com>
2023-11-12 07:07:51 +0000

    my framework

--- users/users.rb
+++ users/users.rb
@@ -7,8 +7,34 @@ before do
   @info = YAML.load_file('users.yaml')
 end
 
-get "/" do
-  @title = "The Adventures of Sherlock Holmes"
+helpers do
+  def count_interests
+    count = 0
+    @info.each do |user, info|
+      count += info[:interests].size
+    end
+    count
+  end
 
+  def count_users
+    @info.keys.size
+  end
+end
+
+get "/" do
   erb :home
 end
+
+get "/:user" do
+  @user = params[:user].to_sym
+
+  redirect "/" if !@info[@user]
+
+  @email = @info[@user][:email]
+  @interests = @info[@user][:interests]
+
+  erb :user
+end
+
+
+
