a61db8207b1a14673ccbb7089de8aa0dbe7c23a2
Lisa Lan <12lisalan@gmail.com>
2023-11-18 02:07:26 +0000

    error messages

--- sinatra_todos_r20160624/sinatra_todos/todo.rb
+++ sinatra_todos_r20160624/sinatra_todos/todo.rb
@@ -2,7 +2,58 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
 get "/" do
-  erb "You have no lists.", layout: :layout
+  redirect "/lists"
+end
+
+# GET /lists      -> view all lists
+# GET /lists/new  -> new list form
+# POST /lists     -> create new list
+# GET /lists/1    -> view a single list
+#
+
+# view all lists
+get "/lists" do
+  @lists = session[:lists]
+  erb :lists
+end
+
+# render new list form
+get "/lists/new" do
+  erb :new_list
+end
+
+# return error message if name invalid.
+# return nil if name is valid.
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    "List name must be between 1 and 100 characters."
+  elsif session[:lists].any? {|list| list[:name] == name}
+    "List name must be unique."
+  end
+end
+
+# create new list
+post "/lists" do
+  list_name = params[:list_name].strip
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list
+  else
+    session[:lists] << {name: list_name, todos: []}
+    session[:success] = "The list has been created"
+    redirect "/lists"
+  end
 end
 set :session_secret, SecureRandom.hex(32)
