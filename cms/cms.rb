00a59a185176828d4c5e032e2e9aae6fdd122c2d
Lisa Lan <12lisalan@gmail.com>
2023-12-13 03:24:18 +0000

    test index and txt

--- cms/cms.rb
+++ cms/cms.rb
@@ -7,6 +7,9 @@ configure do
 end
 
 helpers do
+  # def format_text(string)
+  #   string.gsub(/\n/, "<br>")
+  # end
 end
 
 before do
@@ -17,4 +20,10 @@ root = File.expand_path("..", __FILE__)
 get "/" do
   @files = Dir.glob(root + "/data/*").map{|path| File.basename(path)}
   erb :index
+end
+
+get "/:file" do
+  path = root + "/data/" + params[:file]
+  headers["Content-Type"] = "text/plain"
+  File.read(path) if File.readable?(path)
 end
---
+++ cms/test/cms_test.rb
@@ -0,0 +1,32 @@
+ENV["RACK_ENV"] = "test"
+
+require "minitest/autorun"
+require "rack/test"
+
+require_relative "../cms"
+
+class CMSTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_index
+    get "/"
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+
+    files = ["about.txt", "changes.txt", "history.txt"]
+    files.each{|file| assert_includes last_response.body, file}
+  end
+
+  def test_txt
+    get "/history.txt"
+    assert_equal 200, last_response.status
+    assert_equal "text/plain", last_response["Content-Type"]
+
+    text_body = File.read("data/history.txt")
+    assert_includes last_response.body, text_body
+  end
+end
